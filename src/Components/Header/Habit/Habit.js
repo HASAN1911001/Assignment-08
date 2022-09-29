4088a3dfc7c0de12edfadbe76dd9c0cc49797be6
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-09-29 20:53:13 +0600

    Done

--- src/Components/Header/Habit/Habit.js
+++ src/Components/Header/Habit/Habit.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './Habit.css';
 
-const Habit = () => {
+const Habit = (props) => {
+    const {Habit,img,time} = props.habit;
+   
     return(
         <div>
-            <h4>Habit</h4>
-        </div>   
+            <div className ='habit'>
+                <img src={img}></img>
+                <h3>{Habit}</h3>
+                <h6>Time Require: {time} Hours</h6>
+
+                <button onClick={() => props.handleClick(props.habit.time)} className='btn-habit'>
+                    <p>Add to List</p>
+                </button> 
+            </div>
+        </div>  
      );
 };
 
